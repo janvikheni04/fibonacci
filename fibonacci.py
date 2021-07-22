085712a3b972d3b53ee489a3dfa5095fb9c8c5dc
janvikheni04 <83709055+janvikheni04@users.noreply.github.com>
2021-07-22 11:57:35 +0530

    Create fibonacci.py
    It shows..
    if enter 9 then output is 34

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+def FB(n):
+
+    if n < 0:
+        print("Incorrect input")
+ 
+    elif n == 0:
+        return 0
+ 
+    elif n == 1 or n == 2:
+        return 1
+ 
+    else:
+        return FB(n-1) + FB(n-2)
